16af3c0bfa4eb6b46f7beb84d7836a6b1eafd750
waqas zamir <waqas.zamir@lykke.com>
2018-03-21 16:23:26 +0500

    MTT-2410 created integration tests MTT-2414 MTT-2419

--- src/tests/Donut.Tests/Integration/AccountManagement.cs
+++ src/tests/Donut.Tests/Integration/AccountManagement.cs
@@ -11,18 +11,21 @@ namespace Donut.Tests.Integration
     using FluentAssertions;
     using Xunit;
 
+#pragma warning disable CA1001
     public class AccountManagement : IntegrationTest
     {
+        private readonly AssetAccountsHttpClient httpClient;
+
         public AccountManagement(SecurityFixture securityFixture, DonutFixture donutFixture, WebTerminalFixture webTerminalFixture)
             : base(securityFixture, donutFixture, webTerminalFixture)
         {
+            this.httpClient = new AssetAccountsHttpClient("http://localhost:5009", this.Handler);
         }
 
         [Fact]
         public async Task CanAddInvestorAccount()
         {
             // arrange
-            var httpClient = new AssetAccountsHttpClient("http://localhost:5009", this.Handler);
             var expectedAccount = new InvestorAssetAccount
             {
                 AssetAccountId = "accountId",
@@ -53,16 +56,116 @@ namespace Donut.Tests.Integration
 
                             return;
                         }
+                    }
 
                         httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                    }
                 });
 
             // act
-            await httpClient.AddAssetAccountAsync(expectedAccount).ConfigureAwait(false);
+            await this.httpClient.AddAssetAccountAsync(expectedAccount).ConfigureAwait(false);
 
             // assert
             actualAccount.Should().BeEquivalentTo(expectedAccount);
         }
+
+        [Fact]
+        public async Task CanModifyInvestorAccount()
+        {
+            // arrange
+            var expectedAccount = new InvestorAssetAccountBasicInfo
+            {
+                AssetAccountId = "accountId",
+                Type = AssetAccountType.Lykke,
+                Status = AssetAccountStatus.Active,
+                MarginAccount = "marginAccountId",
+                BankIdentificationMargin = "bim",
+                ReferenceAccount = "ra",
+                BankIdentificationReference = "bir",
+                WithdrawalAllowed = true,
+            };
+
+            var actualAccount = default(InvestorAssetAccountBasicInfo);
+
+            // hook into the context (somehow) and verify
+            this.AssignRequestDelegate(
+                async httpContext =>
+                {
+                    if (httpContext.Request.Method.Equals("POST", StringComparison.InvariantCultureIgnoreCase)
+                        && httpContext.Request.Path.Value.Equals($"/api/externalAssetAccount/investor/{expectedAccount.AssetAccountId}", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        actualAccount = await httpContext.Request.DeserializeBody<InvestorAssetAccountBasicInfo>().ConfigureAwait(false);
+
+                        if (actualAccount != null)
+                        {
+                            httpContext.Response.StatusCode = (int)HttpStatusCode.Accepted;
+
+                            return;
+                    }
+                    }
+
+                    httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                });
+
+            // act
+            await this.httpClient.UpdateAssetAccountAsync(expectedAccount).ConfigureAwait(false);
+
+            // assert
+            actualAccount.Should().BeEquivalentTo(expectedAccount);
+        }
+
+        [Fact]
+        public async Task CanCloseAssetAccount()
+        {
+            // arrange
+            var expectedAssetAccountId = "AA1111";
+
+            this.AssignRequestDelegate(
+                httpContext =>
+                {
+                    // The url and method types should match
+                    if (httpContext.Request.Method.Equals("PATCH", StringComparison.InvariantCultureIgnoreCase)
+                        && httpContext.Request.Path.Value.Equals($"/api/externalAssetAccount/{expectedAssetAccountId}/close", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        httpContext.Response.StatusCode = (int)HttpStatusCode.Accepted;
+
+                        return Task.CompletedTask;
+                    }
+
+                    httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                    return Task.CompletedTask;
+                });
+
+            // act
+            await this.httpClient.CloseAsync(expectedAssetAccountId).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task CanTerminateAssetAccount()
+        {
+            // arrange
+            var expectedAssetAccountId = "AA1111";
+
+            this.AssignRequestDelegate(
+                httpContext =>
+                {
+                    // The url and method types should match
+                    if (httpContext.Request.Method.Equals("DELETE", StringComparison.InvariantCultureIgnoreCase)
+                        && httpContext.Request.Path.Value.Equals($"/api/externalAssetAccount/{expectedAssetAccountId}", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        httpContext.Response.StatusCode = (int)HttpStatusCode.Accepted;
+
+                        return Task.CompletedTask;
+                    }
+
+                    httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                    return Task.CompletedTask;
+                });
+
+            // act
+            await this.httpClient.TerminateAsync(expectedAssetAccountId).ConfigureAwait(false);
+        }
+
     }
 }
