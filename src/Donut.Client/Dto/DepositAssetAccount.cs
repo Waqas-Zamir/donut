191df9710d76563df38873ee55ced1431ee642b3
waqas zamir <waqas.zamir@lykke.com>
2018-03-27 17:55:31 +0500

    MTT-2422 created deposit client and console commands

---
+++ src/Donut.Client/Dto/DepositAssetAccount.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+namespace Donut.Client
+{
+    /// <summary>
+    /// Represents Deposit request on asset account
+    /// </summary>
+    public class DepositAssetAccount
+    {
+        /// <summary>
+        /// Gets or sets the asset account id on which the deposit will be made
+        /// </summary>
+        public string AssetAccountId { get; set; }
+
+        /// <summary>
+        /// Gets or sets ISO currency code for this deposit
+        /// </summary>
+        public string SettlementCurrency { get; set; }
+
+        /// <summary>
+        /// Gets or sets decimal precision for this deposit request
+        /// </summary>
+        public int Precision { get; set; }
+
+        /// <summary>
+        /// Gets or sets the amount to be deposited
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique reference identifier for this deposit transaction
+        /// </summary>
+        public string ReferenceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets information about this transaction
+        /// </summary>
+        public string TransactionInfo { get; set; }
+    }
+}
---
+++ src/Donut.Console/Commands/donut-accounts/DepositAssetAccountCommand.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+namespace Donut.Console.Commands
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Donut.Client;
+    using McMaster.Extensions.CommandLineUtils;
+    using Newtonsoft.Json;
+
+    internal class DepositAssetAccountCommand : ICommand
+    {
+        private DepositAssetAccount deposit;
+
+        public static void Configure(CommandLineApplication app, CommandLineOptions options, IConsole console)
+        {
+            // description
+            app.Description = "Deposit to an asset account";
+            app.ExtendedHelpText = $"{Environment.NewLine}Use 'accounts deposit -i' to enter interactive mode{Environment.NewLine}";
+
+            // arguments
+            var argumentAssetAccountId = app.Argument("account", "The asset account id to deposit", false);
+            var argumentReferenceId = app.Argument("reference", "The unique reference identifier for this transaction", false);
+            var argumentAmount = app.Argument("amount", "The amount to deposit", false);
+            var argumentSettlementCurrency = app.Argument("currency", "The ISO currency code for settlement currency", false);
+
+            // options
+            var optionInteractive = app.Option("-i|--interactive", "Enters interactive mode", CommandOptionType.NoValue);
+
+            // action (for this command)
+            app.OnExecute(
+                () =>
+                {
+                    if ((string.IsNullOrWhiteSpace(argumentAssetAccountId.Value)
+                        || string.IsNullOrWhiteSpace(argumentReferenceId.Value)
+                        || string.IsNullOrWhiteSpace(argumentSettlementCurrency.Value)
+                        || string.IsNullOrWhiteSpace(argumentAmount.Value))
+                            && !optionInteractive.HasValue())
+                    {
+                        app.ShowVersionAndHelp();
+                        return;
+                    }
+
+                    if (!decimal.TryParse(argumentAmount.Value, out decimal amount)
+                        && !optionInteractive.HasValue())
+                    {
+                        app.ShowVersionAndHelp();
+                        return;
+                    }
+
+                    var reporter = new ConsoleReporter(console, options.Verbose.HasValue(), false);
+                    var helper = new DepositHelper();
+
+                    var deposit = new DepositAssetAccount
+                    {
+                        AssetAccountId = argumentAssetAccountId.Value,
+                        Amount = amount,
+                        SettlementCurrency = argumentSettlementCurrency.Value,
+                        Precision = string.IsNullOrEmpty(argumentAmount.Value)
+                            ? 0
+                            : argumentAmount.Value
+                            .SkipWhile(c => c != '.')
+                            .Skip(1)
+                            .Count()
+                };
+
+                    reporter.Verbose("Prototype deposit (from command line arguments):");
+                    reporter.Verbose(JsonConvert.SerializeObject(deposit));
+
+                    if (!helper.IsValid(deposit) || optionInteractive.HasValue())
+                    {
+                        try
+                        {
+                            deposit = helper.GetValid(deposit);
+                        }
+                        catch (NotSupportedException ex)
+                        {
+                            throw new CommandParsingException(app, $"Operation Aborted. {ex.Message}", ex);
+                        }
+
+                        reporter.Verbose("Validated deposit (from interactive console):");
+                        reporter.Verbose(JsonConvert.SerializeObject(deposit));
+                    }
+
+                    options.Command = new DepositAssetAccountCommand { deposit = deposit };
+                });
+        }
+
+        public Task ExecuteAsync(CommandContext context) => context.AssetAccountsClient.DepositAsync(this.deposit);
+
+        private static string Safe(string value, string errorMessage)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new NotSupportedException(errorMessage);
+            }
+
+            return value;
+        }
+
+        private class DepositHelper
+        {
+            public DepositAssetAccount GetPrototype(DepositAssetAccount deposit) => deposit;
+
+            public bool IsValid(DepositAssetAccount deposit) =>
+                !string.IsNullOrWhiteSpace(deposit.AssetAccountId)
+                && !string.IsNullOrWhiteSpace(deposit.ReferenceId)
+                && !string.IsNullOrWhiteSpace(deposit.SettlementCurrency)
+                && deposit.Amount > 0;
+
+            public DepositAssetAccount GetValid(DepositAssetAccount deposit)
+            {
+                deposit.AssetAccountId = Safe(Prompt.GetString("Asset Account Id:", deposit.AssetAccountId), "Cannot deposit without specifying account id");
+                deposit.ReferenceId = Safe(Prompt.GetString("Reference Id:", deposit.ReferenceId), "Cannot deposit without specifying reference id");
+                var amountString = Safe(Prompt.GetString("Amount:", string.Format(new NumberFormatInfo() { NumberDecimalDigits = deposit.Precision }, "{0:F}", deposit.Amount)), "Cannot deposit without specifying some amount");
+                deposit.Amount = Convert.ToDecimal(amountString, CultureInfo.InvariantCulture);
+                deposit.SettlementCurrency = Safe(Prompt.GetString("Currency code:", deposit.SettlementCurrency), "Cannot deposit without specifying currency code");
+
+                deposit.Precision = amountString
+                    .Trim()
+                    .SkipWhile(c => c != '.')
+                    .Skip(1)
+                    .Count();
+
+                return deposit;
+            }
+        }
+    }
+}
